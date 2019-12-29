b1e0b015f6fb0050d9c729d9f4aeb56bd5b999d2
MrReds1324 <ryanb27@gmail.com>
2019-12-29 11:51:19 -0500

    Made appear from back play continously Added method to create the task simply Refactored solid color to take an array instead of individual colors Refactored some existing methods to correctly process as RBG instead of RGB

--- PiLightsServer.py
+++ PiLightsServer.py
@@ -41,7 +41,7 @@ def wheel(pos):
 
 
 # Define rainbow cycle function to do a cycle of all hues.
-def rainbow_cycle_successive(pixels, wait=0.1):
+def rainbow_cycle_successive(pixels, wait=0):
     for i in range(pixels.count()):
         # tricky math! we use each pixel as a fraction of the full 96-color wheel
         # (thats the i / strip.numPixels() part)
@@ -100,13 +100,13 @@ def brightness_increase(pixels, steps=256, wait=0.01, step=1):
             time.sleep(wait)
 
 
-def blink_color(pixels, blink_times=5, wait=0.5, color=(255, 0, 0)):
+def blink_color(pixels, blink_times=5, wait=0, color=[255, 0, 0]):
     for i in range(blink_times):
         # blink two times, then wait
         pixels.clear()
         for j in range(2):
             for k in range(pixels.count()):
-                pixels.set_pixel(k, Adafruit_WS2801.RGB_to_color(color[0], color[1], color[2]))
+                pixels.set_pixel(k, Adafruit_WS2801.RGB_to_color(color[0], color[2], color[1]))
                 set_intensity(k)
             pixels.show()
             time.sleep(0.08)
@@ -116,25 +116,24 @@ def blink_color(pixels, blink_times=5, wait=0.5, color=(255, 0, 0)):
         time.sleep(wait)
 
 
-def appear_from_back(pixels, color=(255, 0, 0)):
-    pos = 0
+def appear_from_back(pixels, color=[255, 0, 0], wait=0):
     for i in range(pixels.count()):
         for j in reversed(range(i, pixels.count())):
             pixels.clear()
             # first set all pixels at the begin
             for k in range(i):
-                pixels.set_pixel(k, Adafruit_WS2801.RGB_to_color(color[0], color[1], color[2]))
+                pixels.set_pixel(k, Adafruit_WS2801.RGB_to_color(color[0], color[2], color[1]))
                 set_intensity(k)
             # set then the pixel at position j
-            pixels.set_pixel(j, Adafruit_WS2801.RGB_to_color(color[0], color[1], color[2]))
+            pixels.set_pixel(j, Adafruit_WS2801.RGB_to_color(color[0], color[2], color[1]))
             set_intensity(j)
             pixels.show()
-            time.sleep(0.02)
+            time.sleep(wait)
 
 
-def solid_colors(pixels, r, b, g, wait=0):
+def solid_colors(pixels, color=[255, 0, 0], wait=0):
     for i in range(pixels.count()):
-        pixels.set_pixel(i, Adafruit_WS2801.RGB_to_color(r, b, g))
+        pixels.set_pixel(i, Adafruit_WS2801.RGB_to_color(color[0], color[2], color[1]))
         set_intensity(i)
         pixels.show()
         if wait > 0:
@@ -143,7 +142,7 @@ def solid_colors(pixels, r, b, g, wait=0):
 
 def solid_array(pixels, arr, wait=0):
     for i in range(pixels.count()):
-        pixels.set_pixel(i, Adafruit_WS2801.RGB_to_color(arr[i][0], arr[i][1], arr[i][2]))
+        pixels.set_pixel(i, Adafruit_WS2801.RGB_to_color(arr[i][0], arr[i][2], arr[i][1]))
         set_intensity(i)
         pixels.show()
         if wait > 0:
@@ -199,15 +198,10 @@ def solid_set():
     wait_time = 0
     if req_obj.get('wait'):
         wait_time = req_obj.get('wait')
-    red = 255
-    green = 0
-    blue = 4
+    color = [255, 0, 0]
     if req_obj.get('color'):
         color = req_obj.get('color')
-        red = color[0]
-        green = color[1]
-        blue = color[2]
-    solid_colors(pixels, red, blue, green, wait_time)
+    solid_colors(pixels, color, wait_time)
     return '{"success": True}'
 
 
@@ -253,25 +247,35 @@ def intensity_set():
 @post('/appearfromback')
 def appear_from_back_set():
     req_obj = json.loads(request.body.read())
-    color = (255, 0, 4)
-    if req_obj.get('color'):
-        color = req_obj.get('color')
-    #appear_from_back(pixels, color)
-    QUEUE.put(None)
+    QUEUE.put(build_task('appearfromback', req_obj, True))
     return '{"success": True}'
 
+def build_task(task, body, replay=False):
+    return {'task': task, 'body': body, 'replay': replay}
 
 def worker():
     while True:
         item = QUEUE.get()
-        # This ends the worker thread and sets the last light to be red to indicate the server is not running properly
+        # This ends the worker thread and sets the last light to be red to indicate the server is not running properly1wqa1
         if item is None:
             pixels.clear()
             pixels.set_pixel(30, Adafruit_WS2801.RGB_to_color(255, 0, 0))
             pixels.show()
             print(QUEUE.empty())
             break
+        else:
+            data = item.get('body')
+            color = [255, 0, 4]
+            if data.get('color'):
+                color = data.get('color')
+            wait_time = 0
+            if data.get('wait'):
+                wait_time = data.get('wait')
+            if item.get('task') == "appearfromback":
+                appear_from_back(pixels, color, wait_time)
         QUEUE.task_done()
+        if item.get('replay') and QUEUE.empty():
+            QUEUE.put(item)
 
 
 def start_worker():
